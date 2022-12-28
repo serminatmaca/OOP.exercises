d1364a02c73f0bfcdb80597f2b305717dc162253
Şermin Atmaca <117165693+serminatmaca@users.noreply.github.com>
2022-12-28 21:48:58 +0300

    OOP.exercises arrangement

---
+++ OOP.exercises/Program.cs
@@ -0,0 +1,22 @@
+﻿using OOP.exercises;
+
+
+internal class Program
+{
+    // insan isimli bir sınıf içinde isim, soyisim, boy, kilo, yaş alanlarını tanımlayıp, bir kurucu metot içinde bu alanlar için varsayılan değer atamalarını yapacağız. son olarak da " Tanis" isimli metotla girilen bilgileri kullanıcıya belli, güzel, açıklayıcı bir şekilde görüntüleyen bir program yazınız..
+
+    // örnek: Ben Şermin ATMACA.. boyum: 1,69.. kilom: 67 kg.. yaşım:29'dur
+
+    private static void Main(string[] args)
+
+    {
+        clsPeople people = new clsPeople("sermin", "ATMACA", 169, 67, 29);
+
+        people.Tanis();
+
+
+
+        
+    }
+
+}
---
+++ OOP.exercises/clsPeople.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace OOP.exercises
+{
+	internal class clsPeople
+	{
+
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public int Lenght { get; set; }
+        public int Kilo { get; set; }
+        public int Age { get; set; }
+
+
+        public clsPeople( string Ad, string Soyad, int Boy, int kilo, int Yaş)
+		{
+
+            Name = Ad;
+            LastName = Soyad;
+            Lenght = Boy;
+            Kilo = kilo;
+            Age = Yaş;
+
+
+
+		}
+
+        public void Tanis()
+        {
+            Console.WriteLine("Adım : " + Name + "\n" + " Soyadım : " + LastName + "\n" + "Boyum : " + Lenght + "\n" + "Kilom : " + Kilo + "\n" + "Yaşım : " + Age + "\n"); 
+        }
+
+	}
+}
+
